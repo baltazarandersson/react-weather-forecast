f5de2043d5ab371c73b65ea76f7ab8062ab5baa8
Baltazar <balti.kpo@gmail.com>
2022-03-16 21:11:14 -0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Nav from "./components/Nav";
 import Body from "./components/Body";
+import { Route } from "wouter";
 
 function App() {
   return (
     <div className="App">
-      <Nav></Nav>
-      <Body></Body>
+      <Route path="/location/:keyword" component={Nav} />
+      <Route path="/location/:keyword" component={Body} />
     </div>
   );
 }
