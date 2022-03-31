30652901973a333655c0623d0c691d28e5f7ee9b
Baltazar <balti.kpo@gmail.com>
2022-03-31 11:56:53 -0300

    Improved mobile design

--- src/hooks/useUnitConversor.js
+++ src/hooks/useUnitConversor.js
@@ -3,8 +3,6 @@ import { useUnitContext } from "context/UnitContext";
 export function useUnitConversor(temp) {
   const { unitState } = useUnitContext();
 
-  console.log(temp);
-
   if (unitState === "C")
     return { temp: Math.round(temp * 10) / 10, unit: "°C" };
   else return { temp: Math.round((temp * 1.8 + 32) * 10) / 10, unit: "°F" };
